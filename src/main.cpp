b8c60e67dc2557975410c26ece84dcc582bc57d2
dzarrini <dzarrini@mymail.mines.edu>
2016-04-24 16:08:36 -0600

    run tests

--- src/main.cpp
+++ src/main.cpp
@@ -114,9 +114,9 @@ void run_test() {
 
 	ofstream ofs("simple_data_depths.txt");
 
-	for( int i = 1; i <= 12; i+=1) {
+	for( int i = 1; i <= 10; i+=1) {
 		if( i > 1 && i % 2 != 0 ) continue;
-		for( int j = 1; j <= 12; j+=1) {
+		for( int j = 1; j <= 10; j+=1) {
 			if( j > 1 && j % 2 != 0 ) continue;
 			ofs << "Player 1 Depth: " << i << " ,Player 2 Depth: " << j << endl;
 			for( int z = 0; z < 10; z++) {
