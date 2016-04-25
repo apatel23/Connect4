585d31e805d587123391607d3de3f5a786ba90b6
dzarrini <dzarrini@mymail.mines.edu>
2016-04-24 19:10:08 -0600

    test 3 and 4

--- src/main.cpp
+++ src/main.cpp
@@ -17,9 +17,13 @@ using namespace std;  // We will almost always use the standard namespace.
 
 #include "game/game.h"
 #include "board/ConnectFourBoard.h"
+
 #include "heuristic/heuristic.h"
 #include "heuristic/simple_heuristic.h"
 #include "heuristic/simple_two.h"
+#include "heuristic/simple_three.h"
+#include "heuristic/simple_four.h"
+
 #include "flag_parser/flag_parser.h"
 #include "player/computer_player/ComputerPlayer.h"
 #include "player/human_player/HumanPlayer.h"
@@ -31,6 +35,8 @@ void console();
 
 void run_test();
 void run_test_two();
+void run_test_three();
+void run_test_four();
 
 
 int main( int argc, char** argv)
@@ -92,7 +98,9 @@ int main( int argc, char** argv)
 
 
 	//run_test();
-	run_test_two();
+	//run_test_two();
+	run_test_three();
+	run_test_four();
 
 
 
@@ -245,3 +253,177 @@ void run_test_two() {
 
 	ofs.close();
 }
+
+void run_test_three() {
+	Player* p_one;
+	Player* p_two;
+
+	Heuristic * h_one;
+	Heuristic * h_two;
+
+
+	ConnectFourBoard *theBoard;
+
+	ofstream ofs("simple1_simple3.txt");
+
+	for( int i = 1; i <= 10; i+=1) {
+		if( i > 1 && i % 2 != 0 ) continue;
+
+			ofs << "Simple_1 vs. Simple_3 Depth: " << i << endl;
+			for( int z = 0; z < 25; z++) {
+				Game g;
+				theBoard = new ConnectFourBoard;
+				p_one = new ComputerPlayer(true, theBoard);
+				p_two = new ComputerPlayer(false, theBoard);
+
+				h_one = new SimpleHeuristic(29, false, i);
+				h_two = new SimpleThree(29,false,  i);
+
+				p_one->setHeuristic(h_one);
+				p_two->setHeuristic(h_two);
+
+				g.construct_game(theBoard, p_one, p_two);
+				g.play_game();
+
+
+				char winner = theBoard->getWinner();
+				if( winner == ' ' )
+				{
+					ofs << "-,0,";
+				}
+				else
+				{
+					ofs << winner << ",1,";
+					
+				}
+				ofs << g.numMoves << endl;
+				g.numMoves = 0;
+				delete theBoard;
+			}
+
+			ofs << endl << endl;
+			ofs << "Simple_3 vs. Simple_1 Depth: " << i << endl;
+			for( int z = 0; z < 25; z++) {
+				Game g;
+				theBoard = new ConnectFourBoard;
+				p_one = new ComputerPlayer(true, theBoard);
+				p_two = new ComputerPlayer(false, theBoard);
+
+				h_one = new SimpleHeuristic(29, false, i);
+				h_two = new SimpleThree(29,false,  i);
+
+				p_one->setHeuristic(h_one);
+				p_two->setHeuristic(h_two);
+
+				g.construct_game(theBoard, p_two, p_one);
+				g.play_game();
+
+
+				char winner = theBoard->getWinner();
+				if( winner == ' ' )
+				{
+					ofs << "-,0,";
+				}
+				else
+				{
+					ofs << winner << ",1,";
+					
+				}
+				ofs << g.numMoves << endl;
+				g.numMoves = 0;
+				delete theBoard;
+			}
+			ofs << endl << endl;
+
+	}
+
+	ofs.close();
+}
+
+
+void run_test_four() {
+	Player* p_one;
+	Player* p_two;
+
+	Heuristic * h_one;
+	Heuristic * h_two;
+
+
+	ConnectFourBoard *theBoard;
+
+	ofstream ofs("simple1_simple4.txt");
+
+	for( int i = 1; i <= 10; i+=1) {
+		if( i > 1 && i % 2 != 0 ) continue;
+
+			ofs << "Simple_1 vs. Simple_4 Depth: " << i << endl;
+			for( int z = 0; z < 25; z++) {
+				Game g;
+				theBoard = new ConnectFourBoard;
+				p_one = new ComputerPlayer(true, theBoard);
+				p_two = new ComputerPlayer(false, theBoard);
+
+				h_one = new SimpleHeuristic(29, false, i);
+				h_two = new SimpleFour(29,false,  i);
+
+				p_one->setHeuristic(h_one);
+				p_two->setHeuristic(h_two);
+
+				g.construct_game(theBoard, p_one, p_two);
+				g.play_game();
+
+
+				char winner = theBoard->getWinner();
+				if( winner == ' ' )
+				{
+					ofs << "-,0,";
+				}
+				else
+				{
+					ofs << winner << ",1,";
+					
+				}
+				ofs << g.numMoves << endl;
+				g.numMoves = 0;
+				delete theBoard;
+			}
+
+			ofs << endl << endl;
+			ofs << "Simple_4 vs. Simple_1 Depth: " << i << endl;
+			for( int z = 0; z < 25; z++) {
+				Game g;
+				theBoard = new ConnectFourBoard;
+				p_one = new ComputerPlayer(true, theBoard);
+				p_two = new ComputerPlayer(false, theBoard);
+
+				h_one = new SimpleHeuristic(29, false, i);
+				h_two = new SimpleFour(29,false,  i);
+
+				p_one->setHeuristic(h_one);
+				p_two->setHeuristic(h_two);
+
+				g.construct_game(theBoard, p_two, p_one);
+				g.play_game();
+
+
+				char winner = theBoard->getWinner();
+				if( winner == ' ' )
+				{
+					ofs << "-,0,";
+				}
+				else
+				{
+					ofs << winner << ",1,";
+					
+				}
+				ofs << g.numMoves << endl;
+				g.numMoves = 0;
+				delete theBoard;
+			}
+			ofs << endl << endl;
+
+	}
+
+	ofs.close();
+}
+
